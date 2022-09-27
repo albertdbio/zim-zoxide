5b02f1cbb5e083007d7615b201e9299045dc6d4f
Albert Martinez <ioalbertdb@gmail.com>
2022-09-27 09:33:39 -0700

    Fix command flag placement

--- init.zsh
+++ init.zsh
@@ -1,4 +1,4 @@
 if (( $+commands[zoxide] )); then
-    $commands[zoxide] init zsh >| ${0:A:h}/zoxide.zsh
-    source ${0:A:h}/zoxide.zsh --cmd cd
+    $commands[zoxide] init zsh --cmd cd >| ${0:A:h}/zoxide.zsh
+    source ${0:A:h}/zoxide.zsh
 fi
